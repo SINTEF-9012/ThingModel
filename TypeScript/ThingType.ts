ef843b249fc3f13546a973c1b69e908627b01355
Antoine Pultier <antoine.pultier@sintef.no>
2014-02-05 15:05:33 +0100

    ThingType.ts

--- TypeScript/ThingType.ts
+++ TypeScript/ThingType.ts
@@ -1,17 +1,40 @@
 module ThingModel {
+
 	export class ThingType {
+		private _name: string;
 
+		public get Name(): string {
+			return this._name;
+		}
+
+		public Description: string;
+
+		private _properties: { [key: string]: PropertyType };
+
+		constructor(name: string) {
+			this._name = name;
+			this._properties = {};
+		}
+
+		public Check(thing: Thing): boolean {
+			return (thing.Type === this ||
+				(thing.Type !== null && thing.Type._name === this._name)) &&
+				_.all(this._properties, (propertyType: PropertyType) => {
+						return propertyType.Check(thing.GetProperty<Property>(propertyType.Key));
+					}
+				);
+		}
+
+		public DefineProperty(property: PropertyType): void {
+			this._properties[property.Key] = property;
+		}
+
+		public GetPropertyDefinition(key: string): PropertyType {
+			return this._properties[key];
+		}
+
+		public GetProperties(): PropertyType[] {
+			return _.values(this._properties);
+		}
 	}
-
-//    export class Property {
-//        private _key: string;
-//
-//		public get Key(): string {
-//		    return this._key;
-//        }
-//
-//        constructor(key : string) {
-//            this._key = key;
-//        }
-//	}
 }
