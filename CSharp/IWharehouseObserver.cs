640f2b2da00c85a75483d6e40213c022f18ef407
Antoine Pultier <antoine.pultier@sintef.no>
2014-02-06 17:07:57 +0100

    IWharehouseObserver now in java (lot of engineering)

--- CSharp/IWharehouseObserver.cs
+++ CSharp/IWharehouseObserver.cs
@@ -5,6 +5,6 @@
         void New(Thing thing);
         void Deleted(Thing thing);
         void Updated(Thing thing);
-        void Define(ThingType thing);
+        void Define(ThingType thingType);
     }
 }
---
+++ Java/src/main/java/org/thingmodel/IWharehouseObserver.java
@@ -0,0 +1,8 @@
+package org.thingmodel;
+
+public interface IWharehouseObserver {
+	void New(Thing thing);
+	void Deleted(Thing thing);
+	void Updated(Thing thing);
+	void Define(ThingType thingType);
+}
