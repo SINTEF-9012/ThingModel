ef4047efe9c9902131c7ed4838c41185e4eea17b
Antoine Pultier <antoine.pultier@sintef.no>
2014-02-12 12:50:54 +0100

    Unit tests in Java with Maven and IntelliJ

---
+++ Java/src/test/java/org/thingmodel/ThingTest.java
@@ -0,0 +1,142 @@
+package org.thingmodel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ThingTest {
+
+    private Thing _thing;
+    private ThingType _type;
+    private Thing _otherThing;
+    private Property _name;
+
+    @Before
+    public void setUp() throws Exception {
+        _type = new ThingType("rabbit");
+        _type.DefineProperty(new PropertyType("name", Property.String.class));
+
+        _thing = new Thing("Groaw", _type);
+
+        _otherThing = new Thing("Gobiwa");
+        _thing.Connect(_otherThing);
+
+        _name = new Property.String("name", "Alphonse");
+        _thing.setProperty(_name);
+    }
+
+    @Test
+    public void testWrongID() throws Exception {
+
+        try {
+
+            new Thing(null);
+            Assert.fail();
+        } catch (Exception e) {
+
+        }
+
+        try {
+
+            new Thing(null);
+            Assert.fail();
+        } catch (Exception e) {
+
+        }
+
+
+    }
+
+    @Test
+    public void testSetProperties() throws Exception {
+        _thing.setProperty(new Property.Integer("speed", 15));
+        _thing.setProperty(new Property.Double("speed", 15.0));
+        Assert.assertNotNull(_thing.getProperty("speed"));
+    }
+
+    @Test
+    public void testGetProperty() throws Exception {
+        Assert.assertNotNull(_thing.getProperty("name", Property.String.class));
+        Assert.assertNotNull(_thing.getProperty("name"));
+        Assert.assertNotNull(_thing.getProperty("name", null));
+
+        Assert.assertNull(_thing.getProperty("name", Property.class));
+        Assert.assertNull(_thing.getProperty("name", Property.Double.class));
+        Assert.assertNull(_thing.getProperty("this is a wrong key", Property.String.class));
+    }
+
+    @Test
+    public void testHasProperty() throws Exception {
+        Assert.assertTrue(_thing.hasProperty("name"));
+        Assert.assertFalse(_thing.hasProperty("name2"));
+    }
+
+    @Test
+    public void testConnect() throws Exception {
+        Assert.assertTrue(_thing.IsConnectedTo(_otherThing));
+        Assert.assertFalse(_otherThing.IsConnectedTo(_thing));
+
+        _thing.Connect(null);
+        Assert.assertFalse(_thing.IsConnectedTo(null));
+
+        _thing.Disconnect(null);
+    }
+
+    @Test
+    public void testDisconnect() throws Exception {
+        _thing.Disconnect(_otherThing);
+        Assert.assertFalse(_thing.IsConnectedTo(_otherThing));
+    }
+
+    @Test
+    public void testDisconnectAll() throws Exception {
+        _thing.DisconnectAll();
+        Assert.assertFalse(_thing.IsConnectedTo(_otherThing));
+    }
+
+    @Test
+    public void testGetConnectedThings() throws Exception {
+        for (Thing t : _thing.getConnectedThings()) {
+            _thing.Disconnect(t);
+        }
+
+        Assert.assertFalse(_thing.IsConnectedTo(_otherThing));
+    }
+
+    @Test
+    public void testGetConnectedThingsCount() throws Exception {
+        Assert.assertEquals(_thing.getConnectedThingsCount(), 1);
+        _thing.Disconnect(_otherThing);
+        Assert.assertEquals(_thing.getConnectedThingsCount(), 0);
+    }
+
+    @Test
+    public void testGetProperties() throws Exception {
+        for (Property p : _thing.getProperties()) {
+            Assert.assertEquals(p.getKey(), "name");
+        }
+    }
+
+    @Test
+    public void testConnectingThingtoItself() throws Exception {
+        try {
+            _thing.Connect(_thing);
+            Assert.fail();
+        } catch (Exception e) {}
+
+        try {
+            _thing.Connect(new Thing(_thing.getId()));
+            Assert.fail();
+        } catch (Exception e) {}
+    }
+
+    @Test
+    public void testCompareSameThing() throws Exception {
+        Assert.assertTrue(_thing.Compare(_thing));
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+
+    }
+}
