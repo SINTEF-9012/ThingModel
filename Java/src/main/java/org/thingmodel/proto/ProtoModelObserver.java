e020340c6a090c3f3de2e58cc0e37dcf189599a8
Antoine Pultier <antoine.pultier@sintef.no>
2014-02-13 15:51:50 +0100

    Conversion to HashMap

--- Java/src/main/java/org/thingmodel/proto/ProtoModelObserver.java
+++ Java/src/main/java/org/thingmodel/proto/ProtoModelObserver.java
@@ -1,20 +1,20 @@
 package org.thingmodel.proto;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
 import org.thingmodel.IWharehouseObserver;
 import org.thingmodel.Thing;
 import org.thingmodel.ThingType;
 import org.thingmodel.proto.ProtoTransaction.Transaction;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ProtoModelObserver implements IWharehouseObserver {
 	
-	private Object _lock = new Object();
-	public HashSet<Thing> Updates = new HashSet<>();
-	public HashSet<Thing> Deletions = new HashSet<>();
-	public HashSet<ThingType> Definitions = new HashSet<>();
+	private final Object _lock = new Object();
+	public HashMap<String,Thing> Updates = new HashMap<>();
+	public HashMap<String,Thing> Deletions = new HashMap<>();
+	public HashMap<String,ThingType> Definitions = new HashMap<>();
 
 	public void Reset() {
 		synchronized (_lock) {
@@ -27,29 +27,31 @@ public class ProtoModelObserver implements IWharehouseObserver {
 	@Override
 	public void New(Thing thing) {
 		synchronized (_lock) {
-			Updates.add(thing);
+			Updates.put(thing.getId(), thing);
 		}
 	}
 
 	@Override
 	public void Deleted(Thing thing) {
 		synchronized (_lock) {
-            Updates.remove(thing);
-			Deletions.add(thing);
+            Updates.remove(thing.getId());
+            // We delete it even it's a new thing because this case is not often
+            // and it's more efficient to use only one HashSet for updates and creations
+			Deletions.put(thing.getId(), thing);
 		}
 	}
 
 	@Override
 	public void Updated(Thing thing) {
 		synchronized (_lock) {
-			Updates.add(thing);
+			Updates.put(thing.getId(), thing);
 		}
 	}
 
 	@Override
 	public void Define(ThingType thingType) {
 		synchronized (_lock) {
-			Definitions.add(thingType);
+			Definitions.put(thingType.getName(), thingType);
 		}
 	}
 
@@ -65,9 +67,9 @@ public class ProtoModelObserver implements IWharehouseObserver {
 		List<ThingType> copyDefinitions;
 		
 		synchronized (_lock) {
-			copyUpdates = new ArrayList<>(Updates);
-			copyDeletions = new ArrayList<>(Deletions);
-			copyDefinitions = new ArrayList<>(Definitions);
+			copyUpdates = new ArrayList<>(Updates.values());
+			copyDeletions = new ArrayList<>(Deletions.values());
+			copyDefinitions = new ArrayList<>(Definitions.values());
 		}
 		
 		return toProtobuf.Convert(copyUpdates, copyDeletions, copyDefinitions, senderID);
