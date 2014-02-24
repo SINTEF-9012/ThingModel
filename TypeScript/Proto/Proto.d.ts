6888ef7e5583eeb0aa837c4637c8f649f241c488
Antoine Pultier <antoine.pultier@sintef.no>
2014-02-24 14:51:22 +0100

    Use camelCase getter and setters

--- TypeScript/Proto/Proto.d.ts
+++ TypeScript/Proto/Proto.d.ts
@@ -22,13 +22,29 @@ declare module ThingModel.Proto {
 
 	export interface Property extends ProtoBufModel {
 		string_key: number;
+		getStringKey() : number;
+		setStringKey(stringKey : number): void;
 		type: Property.Type;
+		getType() : Property.Type;
+		setType(type : Property.Type): void;
 		location_value?: Property.Location;
+		getLocationValue() : Property.Location;
+		setLocationValue(locationValue : Property.Location): void;
 		string_value?: Property.String;
+		getStringValue() : Property.String;
+		setStringValue(stringValue : Property.String): void;
 		double_value?: number;
+		getDoubleValue() : number;
+		setDoubleValue(doubleValue : number): void;
 		int_value?: number;
+		getIntValue() : number;
+		setIntValue(intValue : number): void;
 		boolean_value?: boolean;
+		getBooleanValue() : boolean;
+		setBooleanValue(booleanValue : boolean): void;
 		datetime_value?: number;
+		getDatetimeValue() : number;
+		setDatetimeValue(datetimeValue : number): void;
 		
 	}
 	
@@ -49,10 +65,20 @@ declare module ThingModel.Proto.Property {
 
 	export interface Location extends ProtoBufModel {
 		x: number;
+		getX() : number;
+		setX(x : number): void;
 		y: number;
+		getY() : number;
+		setY(y : number): void;
 		z?: number;
+		getZ() : number;
+		setZ(z : number): void;
 		string_system?: number;
+		getStringSystem() : number;
+		setStringSystem(stringSystem : number): void;
 		z_null?: boolean;
+		getZNull() : boolean;
+		setZNull(zNull : boolean): void;
 		
 	}
 	
@@ -70,7 +96,11 @@ declare module ThingModel.Proto.Property {
 
 	export interface String extends ProtoBufModel {
 		value?: string;
+		getValue() : string;
+		setValue(value : string): void;
 		string_value?: number;
+		getStringValue() : number;
+		setStringValue(stringValue : number): void;
 		
 	}
 	
@@ -103,10 +133,20 @@ declare module ThingModel.Proto {
 
 	export interface PropertyType extends ProtoBufModel {
 		string_key: number;
+		getStringKey() : number;
+		setStringKey(stringKey : number): void;
 		type: PropertyType.Type;
+		getType() : PropertyType.Type;
+		setType(type : PropertyType.Type): void;
 		required: boolean;
+		getRequired() : boolean;
+		setRequired(required : boolean): void;
 		string_name?: number;
+		getStringName() : number;
+		setStringName(stringName : number): void;
 		string_description?: number;
+		getStringDescription() : number;
+		setStringDescription(stringDescription : number): void;
 		
 	}
 	
@@ -138,7 +178,11 @@ declare module ThingModel.Proto {
 
 	export interface StringDeclaration extends ProtoBufModel {
 		value: string;
+		getValue() : string;
+		setValue(value : string): void;
 		key: number;
+		getKey() : number;
+		setKey(key : number): void;
 		
 	}
 	
@@ -156,10 +200,20 @@ declare module ThingModel.Proto {
 
 	export interface Thing extends ProtoBufModel {
 		string_id: number;
+		getStringId() : number;
+		setStringId(stringId : number): void;
 		string_type_name: number;
+		getStringTypeName() : number;
+		setStringTypeName(stringTypeName : number): void;
 		properties: Property[];
+		getProperties() : Property[];
+		setProperties(properties : Property[]): void;
 		connections: number[];
+		getConnections() : number[];
+		setConnections(connections : number[]): void;
 		connections_change?: boolean;
+		getConnectionsChange() : boolean;
+		setConnectionsChange(connectionsChange : boolean): void;
 		
 	}
 	
@@ -177,8 +231,14 @@ declare module ThingModel.Proto {
 
 	export interface ThingType extends ProtoBufModel {
 		string_name: number;
+		getStringName() : number;
+		setStringName(stringName : number): void;
 		string_description?: number;
+		getStringDescription() : number;
+		setStringDescription(stringDescription : number): void;
 		properties: PropertyType[];
+		getProperties() : PropertyType[];
+		setProperties(properties : PropertyType[]): void;
 		
 	}
 	
@@ -196,10 +256,20 @@ declare module ThingModel.Proto {
 
 	export interface Transaction extends ProtoBufModel {
 		string_declarations: StringDeclaration[];
+		getStringDeclarations() : StringDeclaration[];
+		setStringDeclarations(stringDeclarations : StringDeclaration[]): void;
 		things_publish_list: Thing[];
+		getThingsPublishList() : Thing[];
+		setThingsPublishList(thingsPublishList : Thing[]): void;
 		things_remove_list: number[];
+		getThingsRemoveList() : number[];
+		setThingsRemoveList(thingsRemoveList : number[]): void;
 		thingtypes_declaration_list: ThingType[];
+		getThingtypesDeclarationList() : ThingType[];
+		setThingtypesDeclarationList(thingtypesDeclarationList : ThingType[]): void;
 		string_sender_id: number;
+		getStringSenderId() : number;
+		setStringSenderId(stringSenderId : number): void;
 		
 	}
 	
